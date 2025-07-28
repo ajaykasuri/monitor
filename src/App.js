d35c8ceee45f0ce84ec6020be666bf40c6e2d334
Ajay Kasuri <ajay.kasuri@globalsparktek.com>
2025-07-28 16:27:06 +0530

    Fix routing with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
-import './App.css';
+import "./App.css";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route, Navigate, Routes } from 'react-router-dom';
-import Login from './login';
-import FirstPage from './Admin/Firstpage';
-import ATimeSlot from './Admin/ATimeslot';
-import ExamPage from './Student/ExamPage';
-import StudentPage from './Student/StudentPage';
-import Questions from './Admin/Questions';
-import DQuestions from './Student/DQuestions';
-import AdminDashboard from './Admin/AdminDashboard';
-import StudentDashBoard from './Student/StudentDashBoard';
-
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import Login from "./login";
+import FirstPage from "./Admin/Firstpage";
+import ATimeSlot from "./Admin/ATimeslot";
+import ExamPage from "./Student/ExamPage";
+import StudentPage from "./Student/StudentPage";
+import Questions from "./Admin/Questions";
+import DQuestions from "./Student/DQuestions";
+import AdminDashboard from "./Admin/AdminDashboard";
+import StudentDashBoard from "./Student/StudentDashBoard";
 
 function App() {
   return (
-    <HashRouter basename='monitor'>
-    
-
+    <BrowserRouter basename="monitor">
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path="/" element={<Login />} />
         {/* <Route path='/' element={<Spinner />} /> */}
-        <Route path='/firstPage' element={<FirstPage />} />
-        <Route path='/firstPage/atimeslot' element={<ATimeSlot/>} />
-        <Route path='/studentpage' element={<StudentPage/>} />
+        <Route path="/firstPage" element={<FirstPage />} />
+        <Route path="/firstPage/atimeslot" element={<ATimeSlot />} />
+        <Route path="/studentpage" element={<StudentPage />} />
         {/* <Route path='/' element={<ExamPage/>} /> */}
-        <Route path='/studentpage/exampage' element={<ExamPage/>} />
-        <Route path='/dquestions' element={<DQuestions/>} />
-        <Route path='/questions' element={<Questions/>} />
-        <Route path='/admindashboard' element={<AdminDashboard/>} />
-        <Route path='/studentDashboard'element={<StudentDashBoard/>}/>
-
-
-        
-
-
-
-
+        <Route path="/studentpage/exampage" element={<ExamPage />} />
+        <Route path="/dquestions" element={<DQuestions />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/studentDashboard" element={<StudentDashBoard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
